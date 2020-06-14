3107f1a16f30ecc3f20628f5153dad140814878a
tamerl <tamer.mikhaiel@gmail.com>
2020-06-14 21:17:35 +1000

    refactoring the unit test again

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec/autorun'
 require './src/game'
+require 'pry'
 
 
 describe 'Game' do
@@ -12,131 +13,141 @@ describe 'Game' do
 
   describe '.initialize' do
 
-    it 'creates an instance of Game by passing instances of 2 players and 1 board' do
-      expect(Game.new(board: @board, player1: @player1, player2: @player2))
-    end
-
-    # it 'raise an error if the board is missing' do
-    #   expect do
-    #     Game.new(player1: @player1, player2: @player2)
-    #   end.to raise_error("unable to create a game without a board")
-    # end
-    #
-    # it 'raise an error if a player is missing' do
-    #   expect do
-    #     Game.new(board: @board, player2: @player2)
-    #   end.to raise_error("unable to create a game, need one more player")
-    #
-    #   expect do
-    #     Game.new(board: @board, player1: @player1)
-    #   end.to raise_error("unable to create a game, need one more player")
-    # end
-    #
-    # it 'raise an error if more than 2 players are passed' do
-    #   @player3 = double("Player")
-    #   expect do
-    #     Game.new(board: @board, player1: @player1, player2: @player2, player3: @player3)
-    #   end.to raise_error("only 2 players can play the game")
-    # end
-
-    it 'returns the player1 instance when calling @game.player1' do
-      @game = Game.new(board: @board, player1: @player1, player2: @player2)
+    it 'returns the instance variables when calling them through @game.variable' do
       expect(@game.player1).to eq(@player1)
-    end
-
-    it 'returns the player2 instance when calling @game.player2' do
-      @game = Game.new(board: @board, player1: @player1, player2: @player2)
       expect(@game.player2).to eq(@player2)
-    end
-
-    it 'returns the board instance when calling @game.board' do
-      @game = Game.new(board: @board, player1: @player1, player2: @player2)
       expect(@game.board).to eq(@board)
-    end
-
-    it 'returns the player_marker hash of the game when calling @game.player_marker' do
-      expect(@game.player_marler).to eq({:player1=>"x", :player2=>"o"})
+      expect(@game.player_marker).to eq({:player1=>"x", :player2=>"o"})
     end
   end
 
   describe '#write_onboard' do
-
     it "will write 'x' on the board grid when player1 plays" do
       @game.write_onboard(@player1,4)
+      #000
+      #0x0
+      #000
       expect(@board.grid).to eq([[0,0,0],[0,'x',0],[0,0,0]])
     end
 
-    it "will write 'x' on the board grid when player1 plays" do
+    it "will write 'o' on the board grid when player2 plays" do
+      allow(@board).to receive(:grid).and_return([[0,0,0],[0,'x',0],[0,0,0]])
       @game.write_onboard(@player2,5)
+      #000
+      #0xo
+      #000
       expect(@board.grid).to eq([[0,0,0],[0,'x','o'],[0,0,0]])
     end
 
-    it "raise an error if a player try to write on a place that was already written at" do
+    it "raise an error if a player tries to write on a place that was already written at" do
+      allow(@board).to receive(:grid).and_return([[0,0,0],[0,'x',0],[0,0,0]])
+      #000
+      #0x0
+      #000
       expect do
-        @game.write_onboard(@player2,4))
-      end.to raise_error("This place has been already written at")
+        @game.write_onboard(@player2,4)
+      end.to raise_error("Position 4 has been already written at")
     end
 
-    it "raise an error if a player try to write on an invalid place" do
+    it "raise an error if a player tries to write on an invalid place" do
+      #000
+      #000
+      #000
       expect do
-        @game.write_onboard(@player2,9))
-      end.to raise_error("Invalid location! please select from 0 to 8")
+        @game.write_onboard(@player2,9)
+      end.to raise_error("Invalid position! please select from 0 to 8")
     end
   end
 
   describe '#check_winner' do
-
     context 'check for the winner in the horizontal patterns' do
-      it "returns 'Player1 wins!' when the top line is full of x"
-      @board = double("Board", grid: [['x','x','x'],['o','o',0],[0,0,0]])
-      expect(@game.check_winner).to eq('Player1 wins!')
-    end
+      it "returns 'Player1 wins!' when the top line is full of x" do
+        allow(@board).to receive(:grid).and_return([['x','x','x'],['o','o',0],[0,0,0]])
+        #xxx
+        #oo0
+        #000
+        expect(@game.check_winner).to eq('Player1 wins!')
+      end
 
-    it "returns 'Player2 wins!' when the middle line is full of o" do
-      @board = double("Board", grid: [['x','x',0],['o','o','o'],[0,0,0]])
-      expect(@game.check_winner).to eq('Player2 wins!')
-    end
+      it "returns 'Player2 wins!' when the middle line is full of o" do
+        allow(@board).to receive(:grid).and_return([['x','x',0],['o','o','o'],[0,0,0]])
+        #xx0
+        #ooo
+        #000
+        expect(@game.check_winner).to eq('Player2 wins!')
+      end
 
-    it "returns 'Player2 wins!' when the bottom line is full of o" do
-      @board = double("Board", grid: [['x','x','o'],[0,'x','x'],['o','o','o']])
-      expect(@game.check_winner).to eq('Player2 wins!')
+      it "returns 'Player2 wins!' when the bottom line is full of o" do
+        allow(@board).to receive(:grid).and_return([['x','x','o'],[0,'x','x'],['o','o','o']])
+        #xxo
+        #0xx
+        #ooo
+        expect(@game.check_winner).to eq('Player2 wins!')
+      end
     end
-
 
     context 'check for the winner in the vertical patterns' do
-      it "returns 'Player1 wins!' when the left column is full of x"
-      @board = double("Board", grid: [['x','x','o'],['x','o',0],['x',0,0]])
-      expect(@game.check_winner).to eq('Player1 wins!')
+      it "returns 'Player1 wins!' when the left column is full of x" do
+        allow(@board).to receive(:grid).and_return([['x','x','o'],['x','o',0],['x',0,0]])
+        #xxo
+        #xo0
+        #x00
+        expect(@game.check_winner).to eq('Player1 wins!')
+      end
+
+      it "returns 'Player2 wins!' when the middle column is full of o" do
+        allow(@board).to receive(:grid).and_return([['x','o',0],['o','o','x'],[0,'o',0]])
+        #xo0
+        #oox
+        #0o0
+        expect(@game.check_winner).to eq('Player2 wins!')
+      end
+
+      it "returns 'Player1 wins!' when the right column is full of x" do
+        allow(@board).to receive(:grid).and_return([['o','o','x'],[0,'x','x'],['o','o','x']])
+        #oox
+        #0xx
+        #oox
+        expect(@game.check_winner).to eq('Player1 wins!')
+      end
     end
 
-    it "returns 'Player2 wins!' when the middle column is full of o" do
-      @board = double("Board", grid: [['x','o',0],['o','o','x'],[0,'o',0]])
-      expect(@game.check_winner).to eq('Player2 wins!')
+    context 'check for the winner in the digonals patterns' do
+      it "returns 'Player1 wins!' when the \ diagonal is full of x" do
+        allow(@board).to receive(:grid).and_return([['x','x','o'],['x','x','o'],['o','o','x']])
+        #xxo
+        #xxo
+        #oox
+        expect(@game.check_winner).to eq('Player1 wins!')
+      end
+
+      it "returns 'Player2 wins!' when the / diagonal is full of o" do
+        allow(@board).to receive(:grid).and_return([['x','o','o'],['x','o','x'],['o','x',0]])
+        #xoo
+        #xox
+        #ox0
+        expect(@game.check_winner).to eq('Player2 wins!')
+      end
     end
 
-    it "returns 'Player1 wins!' when the right column is full of x" do
-      @board = double("Board", grid: [['x','o','x'],[0,'x','x'],['o','o','x']])
-      expect(@game.check_winner).to eq('Player1 wins!')
+    context 'when the grid is full and there is no winner' do
+      it "returns 'The game is draw, No one wins!'" do
+        allow(@board).to receive(:grid).and_return([['x','o','x'],['x','o','o'],['o','x','x']])
+        #xox
+        #xoo
+        #oxx
+        expect(@game.check_winner).to eq('The game is draw, No one wins!')
+      end
     end
-  end
-  context 'check for the winner in the digonals patterns' do
-    it "returns 'Player1 wins!' when the \ diagonal is full of x"
-    @board = double("Board", grid: [['x','x','o'],['x','x','o'],['o','o','x']])
-    expect(@game.check_winner).to eq('Player1 wins!')
-  end
 
-  it "returns 'Player2 wins!' when the / diagonal is full of o" do
-    @board = double("Board", grid: [['x','o','o'],['x','o','x'],['o','x',0]])
-    expect(@game.check_winner).to eq('Player2 wins!')
-  end
-
-  it "returns 'The game is draw, No one wins!' when the grid become full with no winner" do
-    @board = double("Board", grid: [['x','o','x'],['x','o','o'],['o','x','x']])
-    expect(@game.check_winner).to eq('The game is draw, No one wins!')
-  end
-
-  it "returns 'Player2 wins!' when the grid become full and there is a winner" do
-    @board = double("Board", grid: [['x','o','o'],['x','o','x'],['o','x','x']])
-    expect(@game.check_winner).to eq('Player2 wins!')
+    context 'when the grid is full and there is a winner, say player2' do
+      it "returns 'Player2 wins!'" do
+        allow(@board).to receive(:grid).and_return([['x','o','o'],['x','o','x'],['o','x','x']])
+        #xoo
+        #xox
+        #oxx
+        expect(@game.check_winner).to eq('Player2 wins!')
+      end
+    end
   end
 end
--- src/game.rb
+++ src/game.rb
@@ -2,15 +2,18 @@
    attr_accessor :board
    attr_reader :player1
    attr_reader :player2
+   attr_reader :player_marker
+
 
   def initialize(board: , player1: , player2: )
     @player1 = player1
     @player2 = player2
     @board = board
+    @player_marker = {player1: 'x', player2: 'o'}
   end
 
-  def write_onboard(name,loc)
-    @board.arr[loc % 3] [loc / 3] = name
-    @board.arr
+  def write_onboard(player,loc)
+    # binding.pry
+    board.grid[loc % 3] [loc / 3] = player_marker[player.to_sym]
   end
 end
